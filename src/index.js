44f0c3e77d0d60d5b4cccde32184bc9a3f061fa8
yiKyo <yikyo666@gmail.com>
2018-08-06 15:57:35 +0800

    添加基础样式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 
 import Routes from './routes';
+import './style.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('app'));
