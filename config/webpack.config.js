c9722b43383773ccb07c28ed11700489fd30a578
yiKyo <yikyo666@gmail.com>
2018-08-06 15:31:44 +0800

    添加loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,3 +1,4 @@
+const autoprefixer = require('autoprefixer');
 const Webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
@@ -11,11 +12,66 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 10000,
+                name: 'static/media/[name].[hash:8].[ext]',
+              },
+            },
+          },
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          },
+          {
+            test: /\.(css|scss)$/,
+            use: [
+              {
+                loader: 'style-loader',
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  modules: true,
+                  camelCase: true,
+                  localIdentName: '[local]--[hash:base64:5]',
+                },
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9',
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          },
+        ],
       },
     ],
   },
