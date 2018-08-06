fec5602dca2d09fb8486d28bfb5bcdd190bb4a64
yiKyo <yikyo666@gmail.com>
2018-08-07 00:32:19 +0800

    修改footer css

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,7 +8,7 @@ import Style from './style.scss';
 
 const Footer = ({ name, url }) => (
   <footer>
-    <div className="container">
+    <div className={Style.container}>
       <div className={Style.siteTitleWrapper}>
         <h1 className={Style.siteTitle}>
           <Link title={name} to={url}>
@@ -16,17 +16,16 @@ const Footer = ({ name, url }) => (
           </Link>
         </h1>
 
-        <Link
-          className={`${Style.buttonSquare} ${Style.buttonJumpTop}`}
-          to="/#"
-        >
+        <Link className={Style.buttonJumpTop} to="/#">
           <FontAwesomeIcon icon={faAngleUp} />
         </Link>
       </div>
 
       <p className={Style.copyright}>
         © 2018 / Published with <Link to="https://ghost.org">Ghost</Link>/
-        <Link to="https://github.com/roryg/ghostwriter">Ghostwriter theme</Link>
+        <Link to="https://github.com/roryg/ghostwriter">
+          React Ghostwriter theme
+        </Link>
         By <Link to="http://yikyo.com">yiKyo</Link>
       </p>
     </div>
