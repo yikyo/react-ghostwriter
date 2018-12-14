77f22c1303eadc1f554b58b9238fcb5eb241cc8f
yiKyo <yikyo666@gmail.com>
2018-12-14 16:34:38 +0800

    重构router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
-import { createBrowserHistory } from 'history';
 import React from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AppContainer } from '../containers';
 
 import Home from './home';
@@ -8,13 +7,13 @@ import Posts from './posts';
 
 export default () => {
   return (
-    <Router history={createBrowserHistory()}>
+    <BrowserRouter>
       <Switch>
         <AppContainer>
           <Route exact={true} path="/" component={Home} />
           <Route path="/posts" component={Posts} />
         </AppContainer>
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 };
